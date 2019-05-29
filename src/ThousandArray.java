9c36503619dbf9d37089fa6b365b4d1f43174436
Zakaria Suleman <zakariasuleman@zs.local>
2019-05-28 19:33:55 -0500

    First Commit

---
+++ src/ThousandArray.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+/**
+ A program that implements an array of 1000 elements. Fill the array with randomly generated integer values between 0 and 1000. Prompt
+ user to enter an integer value (target value) between 0 and 1000 (non-inclusive). Then print the number of times (occurrences)
+ the target value occurs in the array, otherwise print ("Value *target value* does not exist in the array")
+
+
+ @author: Zakaria Suleman
+ @version: 1.0
+ @since: 05/28/2019
+ */
+public class ThousandArray {
+
+    public static void main(String[] args) {
+        ThousandArray ta = new ThousandArray();
+        ta.generateRandomArray();
+    }
+    /**
+     * Generates and fills an array of 1000 elements also prompts user to enter an integer value between 0-1000 and checks if it occurs
+     * in the 1000 elements randomly generated array
+     */
+    public void generateRandomArray(){
+
+        int[] array = new int[1000];
+        for(int i = 0; i < array.length; i++){
+            array[i]= (int)(Math.random()*1000);
+            System.out.println(array[i]);
+        }
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter an integer value between 0 and 1000");
+            int target = input.nextInt();
+            int occurs = 0;
+            for(int i = 0; i<array.length; i++){
+                if(target==array[i]){
+                    occurs++;
+                }
+            }
+            if(occurs>0){
+                System.out.println("Value: "+target+" "+"does exist!");
+            }else{
+                System.out.println("Value: "+target+" "+"does not exist!");
+            }
+    }
+}
